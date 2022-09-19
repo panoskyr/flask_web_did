d17f18300cefb2a0a74ddf7219ea17c4fd6ca119
panoskyr <35936229+panoskyr@users.noreply.github.com>
2022-09-19 12:34:16 +0300

    shows dids in profile. Can create a new did

--- project/main.py
+++ project/main.py
@@ -1,8 +1,10 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from flask_login import login_required, current_user
 from project.models import User,Document
+import json
+from . import db
 
 main = Blueprint('main', __name__)
 
@@ -13,8 +15,27 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    docs=Document.query.filter_by(userId=current_user.id)
+    docs=Document.query.filter_by(userId=current_user.id).all()
     if not docs==None:
         #pass the current user and the documents to his name
         return render_template('profile.html', name=current_user.name,docs=docs)
     return render_template('profile.html', name="No docs found")
+
+@main.route('/profile',methods=["POST"])
+@login_required
+def profile_post():
+    documentName=request.form['documentName']
+    with open('project/did1.json', 'r') as myfile:
+        dict=json.load(myfile)
+
+    newDocument=Document(documentName=documentName,jsonFile=dict)
+    current_user.docs.append(newDocument)
+
+    try:
+        db.session.add(newDocument)
+        db.session.commit()
+        return redirect('/profile'),201
+    except:
+        "Problem with saving document", 404
+
+    
